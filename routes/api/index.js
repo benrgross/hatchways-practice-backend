d6768314f58ef4e1d78b0147fbb84b325bcea820
Ben Gross <benrgross@gmail.com>
2022-01-14 12:08:23 +0000

    added routes in seperate file

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const recipeRoutes = require("./recipes");
+
+// direct router to recipe routes file
+route.use("/recipe".recipeRoutes);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require dependencies express, path and fs
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
 
 //set up express on port 300
 const app = express();
@@ -9,6 +10,9 @@ const PORT = 3000;
 // set up data parsing with express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//use api from ./routes
+app.use("routes");
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT} `);
